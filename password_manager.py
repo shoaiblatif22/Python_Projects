0fb2e7389fdd67b8cd3dc700790e6864d64e6224
Shoaib Latif <shoaiblatif@MASTUDEF1XZ1WFY.lan>
2024-04-10 20:47:15 +0100

    Refactored code and added in functions for view and add

--- password_manager.py
+++ password_manager.py
@@ -1,13 +1,20 @@
 pwd = input("What is the master password? ")
 
+
+def view():
+    pass
+
+def add():
+    pass
+
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower
     if mode =="q":
         break
-    
+
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
-        pass
+        add()
     else:
         print("Invalid mode")
