0f9d261d7e7a40a36c14b63ab70b85e5dff02c9e
Shoaib Latif <shoaiblatif@MASTUDEF1XZ1WFY.lan>
2024-04-10 20:52:09 +0100

    Added in with method to append password file

--- password_manager.py
+++ password_manager.py
@@ -1,11 +1,16 @@
-pwd = input("What is the master password? ")
+master_pwd = input("What is the master password? ")
 
 
 def view():
     pass
 
 def add():
-    pass
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('password.txt', 'a') as f: #with automatically closes the file once used. 'a' appends the file and adds to the end of the existing file or creates a new file if file doesnt exist.
+        
+
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower
