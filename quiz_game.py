d439787eeed8452d1f55be9537cd8f65ca01c612
Shoaib Latif <shoaiblatif@MASTUDEF1XZ1WFY.lan>
2024-04-10 20:15:46 +0100

    Added in further input and quiz question

--- quiz_game.py
+++ quiz_game.py
@@ -1,3 +1,14 @@
 print("Welcome to my computer quiz!") 
 
 playing = input("Do you want to play? ")
+
+if playing == "Yes" or "yes":
+    print("Okay! Let's play")
+else:
+    quit()
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect, try again")
