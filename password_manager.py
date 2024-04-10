5cfb5d4c5912e700108fadc37f78b422082bb91f
Shoaib Latif <shoaiblatif@MASTUDEF1XZ1WFY.lan>
2024-04-10 20:42:16 +0100

    added in while loop with user input

--- password_manager.py
+++ password_manager.py
@@ -1,2 +1,13 @@
 pwd = input("What is the master password? ")
 
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower
+    if mode =="q":
+        break
+    
+    if mode == "view":
+        pass
+    elif mode == "add":
+        pass
+    else:
+        print("Invalid mode")
