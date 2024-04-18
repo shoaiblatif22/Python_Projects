4fd4a6ea2e58af72ec736f0d5f90608f0e10ed6d
Shoaib Latif <shoaiblatif@MASTUDEF1XZ1WFY.lan>
2024-04-18 16:02:26 +0100

    Added in two new methods that creates a new block and adds a new transaction

--- Blockchain_Project/blockchain.py
+++ Blockchain_Project/blockchain.py
@@ -5,4 +5,12 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transaction = []
-        
+    
+    def new_block(self):
+        #creates a new block and adds it to the chain
+        pass
+
+    def new_transaction(self):
+        #adds a new transaction to the list of transactions
+        pass
+    
