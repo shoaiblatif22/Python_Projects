2a38e45d7045d2829cd621b4586b34434c3fc2d8
Shoaib Latif <shoaiblatif@MASTUDEF1XZ1WFY.lan>
2024-04-18 16:05:38 +0100

    Restructured files

--- Password_Manager/password_manager.py
+++ Password_Manager/password_manager.py
@@ -9,17 +9,15 @@ def add():
     pwd = input("Password: ")
 
     with open('password.txt', 'a') as f: #with automatically closes the file once used. 'a' appends the file and adds to the end of the existing file or creates a new file if file doesnt exist.
-        
+        f.write(name + "|" + pwd)
 
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower
-    if mode =="q":
-        break
-
-    if mode == "view":
-        view()
-    elif mode == "add":
+    if mode == "add":
         add()
+    elif mode == "view":
+        view()
     else:
-        print("Invalid mode")
+        quit() 
+        
